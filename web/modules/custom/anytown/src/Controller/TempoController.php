b15397b2b42715d91c387e7be12118dc95b90d3d
Hugo Morais <hugo.morais@frontkom.com>
2024-07-17 11:11:13 +0100

    Add controller

--- web/modules/custom/anytown/src/Controller/TempoController.php
+++ web/modules/custom/anytown/src/Controller/TempoController.php
@@ -1,25 +1,36 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Drupal\anytown\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
 /**
- * Returns responses for Anytown routes.
+ * Controller for anytown.tempo_controller route.
  */
 class TempoController extends ControllerBase {
 
   /**
-   * Returns a simple page.
-   *
-   * @return array
-   *   A simple renderable array.
+   * Builds the response.
    */
-  public function content() {
-    return [
+  public function build(string $style): array {
+    // Style should be one of 'short', or 'extended'. And default to 'short'.
+    $style = (in_array($style, ['short', 'extended'])) ? $style : 'short';
+
+    $build['content'] = [
       '#type' => 'markup',
-      '#markup' => $this->t('Bem-vindo à página de tempo!'),
+      '#markup' => '<p>The weather forecast for this week is sunny with a chance of meatballs.</p>',
     ];
+
+    if ($style === 'extended') {
+      $build['content_extended'] = [
+        '#type' => 'markup',
+        '#markup' => '<p><strong>Extended forecast:</strong> Looking ahead to next week we expect some snow.</p>',
+      ];
+    }
+
+    return $build;
   }
 
 }
